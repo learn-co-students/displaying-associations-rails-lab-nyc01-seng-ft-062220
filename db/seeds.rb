4732f6a3c58c5a70b818900f032b0bee02d83b46
jakemeout <jacobosity@gmail.com>
2020-07-19 21:42:37 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+   
+Artist.create(id:1, name: 'Daft Punk')
+Song.create(title: 'The Grid', artist_id:1)
+Song.create(title: 'Voyager', artist_id:1)
+
+Artist.create(id:2, name: 'Radiohead')
+Song.create(title: 'Paranoid Andriod', artist_id:2)
+Song.create(title: 'Mixamatosis', artist_id:2)
+
+Artist.create(id:3, name: 'Paul Simon')
+Song.create(title: 'Diamonds on the soles of her shoes', artist_id:3)
+Song.create(title: 'Graceland', artist_id:3)
